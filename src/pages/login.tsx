c399429e79838541b807b70838423283ad05a426
PirleaAndrei <pirleaandrei1@yahoo.com>
2022-06-30 14:57:19 +0300

    prima versiune

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,20 +1,66 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import * as ROUTES from '../constants/routes';
 
-export default function Login(){
+
+export default function Login() {
+    useEffect(() => {
+        document.title="Login - Instagram"
+      }, []);
+
+      const [email,setEmail]=useState<string>("");
+      const [password,setPassword]=useState<string>("");
+      const [error,setError]=useState<string>("");
+      const isInvalid:boolean=password==="" || email==="";
+    
     return (
-        <div>
-            <div>
-
+        <div className="container flex mx-auto max-w-screen-md items-center h-screen">
+            <div className="flex w-3/5">
+                <img src="/images/login-left.png" alt="iPhone with Instagram app" />
+                
             </div>
-            <div>
-                <div>
-
-                </div>
-                <div>
-                    
+            <div className="flex flex-col w-2/5">
+            <div className="flex flex-col items-center bg-white p-4 border mb-4">
+                <h1 className="flex justify-center w-full">
+                    <img src="/images/login-logo-right.png" alt="Instagram" className="mt-2 w-6/12 mb-4" />
+                </h1>
+                
+                <form method="POST">
+                    <input
+                        aria-label="Enter your email address"
+                        className="text-sm w-full mr-3 py-5 px-4 h-2 border rounded mb-2"
+                        type="text"
+                        placeholder="Email address"
+                        onChange={({target})=>setEmail(target.value)}
+                        value={email}
+                    />
+                    <input
+                        aria-label="Enter your password"
+                        className="text-sm w-full mr-3 py-5 px-4 h-2 border rounded mb-2"
+                        type="password"
+                        placeholder="Password"
+                        onChange={({target})=>setPassword(target.value)}
+                        value={password}
+                    />
+                    <button
+                        disabled={isInvalid}
+                        type="submit"
+                        className={`bg-blue-500 text-white w-full rounded h-8 font-bold${isInvalid && " cursor-not-allowed opacity-50 "}`}
+                    >
+                        Log In
+                        
+                    </button>
+                </form>
+            </div>
+            <div className="flex justify-center items-center flex-col w-full bg-white p-4 border">
+                    <p className="text-sm">
+                        Don't have an account?{' '}
+                        <Link to={ROUTES.SIGN_UP} className="font-bold">
+                            Sign up
+                        </Link>
+                    </p>
                 </div>
             </div>
         </div>
-       
     )
 }
